1ed5f7c7d8661fe947277590a724b06628bcf84c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-08 16:47:49 +0000

    create DB-Engine and requests directorys

---
+++ src/requests/car-specs-request.ts
@@ -0,0 +1,6 @@
+import fetch from 'node-fetch';
+import { specsAPIkey } from '../../config/private-config';
+
+const response = fetch('https://api.api-ninjas.com/v1/cars?limit=1&make=nissan&year=1990&model=300zx', { headers: {'X-Api-Key': specsAPIkey }})
+  .then((res: any) => res.json().then((data: any) => console.log(data)))
+  .catch((error) => console.log(error));
