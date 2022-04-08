088101b3e435af38590a49fffaff08590c089df9
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-08 16:55:23 +0000

    added mongoose connection to application

--- src/App/app.ts
+++ src/App/app.ts
@@ -1,16 +1,45 @@
-import express, {Application, Request, Response} from 'express';
+import express, {Application, NextFunction, Request, Response} from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import { mongoURI } from '../../config/private-config';
 
 /*Routers*/
 import { specsRouter } from './routes/specs';
 
+/* Mongoose setup */
+mongoose.connect(mongoURI);
+
+const DB = mongoose.connection
+
+DB.once('open', _ => {
+  console.log('get-gas-api-cluster is connected !')
+});
+
+DB.on('error', err => {
+  console.error('get-gest-api-cluster cannot connect !(connection error):', err)
+});
+
+/* App setup */
 const app: Application = express();
+const PORT = process.env.PORT || 5555;
+
 
 /* API Homepage */
 app.get('/', (req: Request, res: Response) => {
     res.status(200).sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+/* use for '/specs' route and router */
 app.use('/specs', specsRouter);
 
-app.listen(5555, () => console.log('server is running'));
+/* Error Handlers */
+app.use(function(req: Request, res: Response) {
+    res.status(404).send('404: Page not found');
+});
+
+app.use(function(error: Error, req: Request, res: Response, next: NextFunction) {
+    res.status(500).send('500: Internal error' + error);
+});
+/*==================================================*/
+
+app.listen(PORT, () => console.log(`get-gas-api is running on port ${PORT}`));
