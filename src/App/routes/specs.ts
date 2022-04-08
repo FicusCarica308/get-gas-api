61a115114a660a439da1d5f3670a8ae06eef2907
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-08 16:55:56 +0000

    added comments and a few new api routes

--- src/App/routes/specs.ts
+++ src/App/routes/specs.ts
@@ -1,21 +1,47 @@
 import express, { Router, Request, Response } from 'express';
-import fetch from 'node-fetch';
 
 /* All /specs routes */
+
+/*
+Returns an object with reqeusted info 
+{
+  Always returned:
+  make: [String - make of the vehicle],
+  model: [String - model of the vehicle],
+  year: [String - year of the vehicle],
+
+  optional (returns either one or all):
+  cityMPG: [int],
+  highwayMPG: [int],
+  combinedMPG: [int],
+  octaneRating: [int],
+} 
+*/
+
 const specsRouter: Router = express.Router();
 
-specsRouter.get('/mpg/:make/:mode/:year', (req: Request, res: Response) => {
+/* Returns always returned values + cityMPG */
+specsRouter.get('/city-mpg', (req: Request, res: Response) => {
     console.log('junk');
     res.send('<h1>junk<\h1>');
 });
 
-specsRouter.get('/test', (req: Request, res: Response) => {
+/* Returns always returned values + highwayMPG */
+specsRouter.get('/highway-mpg', (req: Request, res: Response) => {
+    console.log('junk');
+    res.send('<h1>junk<\h1>');
 });
 
-specsRouter.use(function(req, res, next) {
-    let err = new Error(`${req.originalUrl} does not exist`);
-    console.log(err);
-    res.status(404).send(String(err) + '');
+/* Returns always returned values + combinedMPG */
+specsRouter.get('/combined-mpg', (req: Request, res: Response) => {
 });
+
+specsRouter.get('/all-mpg', (req: Request, res: Response) => {
+});
+
+specsRouter.get('/all-fuel-specs', (req: Request, res: Response) => {
+});
+
+specsRouter.get('/')
 
 export { specsRouter };
