452a90ba66a75993813d55a3caa8aca6bb5a7969
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-09 00:12:39 +0000

    misc file renames and directory renames

--- src/Request-Handlers/car-specs-request.ts
+++ src/Request-Handlers/car-specs-request.ts
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import { specsAPIkey } from '../../config/private-config';
+import { specsAPIkey } from '../private-config';
 
 const response = fetch('https://api.api-ninjas.com/v1/cars?limit=1&make=nissan&year=1990&model=300zx', { headers: {'X-Api-Key': specsAPIkey }})
   .then((res: any) => res.json().then((data: any) => console.log(data)))
