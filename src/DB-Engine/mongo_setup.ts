87e1195df8662c5ccbf1beb86e61f5b2acc7e099
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-09 00:10:44 +0000

    added type for connectDB() function parameters
    Also added a new console.log to log if th database had been reconnected

--- src/DB-Engine/mongo_setup.ts
+++ src/DB-Engine/mongo_setup.ts
@@ -1,17 +1,26 @@
 import { mongoURI } from '../private-config';
 import mongoose, { Connection } from 'mongoose';
 
+type ConnectDB = {
+  DBisConnected: Boolean
+  wasVerification: Boolean
+}
+
 /*
 * Description - Handles the initial connection to my mongoDB Atlas Cluster.
 * Arguments: DBisConnected (Whether or not the connection has been made already or not)
 * Other: will not connect to database if already connected
 */
-async function connectDB(DBisConnected: Boolean): Promise<Boolean> {
-  if (DBisConnected == false)
+async function connectDB(values: ConnectDB): Promise<Boolean> {
+  if (values.DBisConnected == false)
   {
     try {
         await mongoose.connect(mongoURI);
-        console.log('get-gas-api-cluster (Initially Connected)')
+        if (values.wasVerification == true) {
+          console.log('get-gas-api-cluster required Reconnect (Reconnected)')
+        } else {
+          console.log('get-gas-api-cluster (Initially Connected)')
+        }
         return (true);
       } catch (error: any) {
         console.error(`get-gest-api-cluster (Could Not Connect) - ${error} - Location: /src/DB-Engine/mongo_setup in connectDB() function`);
