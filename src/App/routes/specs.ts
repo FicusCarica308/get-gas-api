53f1cf39c33bd9c5f76f6814bd45b671a571475f
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-09 00:09:41 +0000

    added a mock-up/boilerplate type route for testing database connection verification

--- src/App/routes/specs.ts
+++ src/App/routes/specs.ts
@@ -1,4 +1,5 @@
 import express, { Router, Request, Response } from 'express';
+import { connectDB } from '../../DB-Engine/mongo_setup';
 
 /* All /specs routes */
 
@@ -21,25 +22,38 @@ Returns an object with reqeusted info
 const specsRouter: Router = express.Router();
 
 /* Returns always returned values + cityMPG */
-specsRouter.get('/city-mpg', (req: Request, res: Response) => {
-    console.log('junk');
-    res.send('<h1>junk<\h1>');
+specsRouter.get('/city-mpg/:make/:model/:year/:cylinders?/:displacment?', (req: Request, res: Response) => {
+  connectDB({ DBisConnected: req.app.locals.DBisConnected, wasVerification: true })
+    .then((result: Boolean) => {
+      req.app.locals.DBisConnected = result; /* Sets global DBisConnected to new connection status */
+      if (result == true) {
+        /* RUN DATABASE QUERY */
+        console.log('was true');
+        res.send('<h1>was true<\h1>');
+        /* IF DATABASE QUERY RETURNS NOTHING RUN REQUEST CALL */
+      } else {
+        /* RUN REQUEST CALL */
+        console.log('was false');
+        res.send('<h1>was false<\h1>');
+      }
+    });
 });
 
 /* Returns always returned values + highwayMPG */
-specsRouter.get('/highway-mpg', (req: Request, res: Response) => {
-    console.log('junk');
-    res.send('<h1>junk<\h1>');
+specsRouter.get('/highway-mpg/:make/:model/:year/:cylinders?/:displacment?', (req: Request, res: Response) => {
+  /* connectDB(req.app.locals.DBisConnected, false); */
+  console.log('junk');
+  res.send('<h1>junk<\h1>');
 });
 
 /* Returns always returned values + combinedMPG */
-specsRouter.get('/combined-mpg', (req: Request, res: Response) => {
+specsRouter.get('/combined-mpg/:make/:model/:year/:cylinders?/:displacment?', (req: Request, res: Response) => {
 });
 
-specsRouter.get('/all-mpg', (req: Request, res: Response) => {
+specsRouter.get('/all-mpg/:make/:model/:year/:cylinders?/:displacment?', (req: Request, res: Response) => {
 });
 
-specsRouter.get('/all-fuel-specs', (req: Request, res: Response) => {
+specsRouter.get('/all-fuel-specs/:make/:model/:year/:cylinders?/:displacment?', (req: Request, res: Response) => {
 });
 
 specsRouter.get('/')
